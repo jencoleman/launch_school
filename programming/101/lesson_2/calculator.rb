cc03a0c3ab579ae5e97e0fd5b55794b8a15b42ec
Jen Coleman <progressive.jen@gmail.com>
2016-02-11 13:59:36 +0000

    calculator lesson update

--- programming/101/lesson_2/calculator.rb
+++ programming/101/lesson_2/calculator.rb
@@ -1,96 +1,95 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+puts MESSAGES.inspect
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def validate_number?(i)
-  i.to_i() != 0
+def validate_number?(number)
+  number.to_i.to_s == number || number.to_f.to_s == number
+end
+
+def number?(num)
+  
 end
 
 def oper_to_message(op)
-  case op.downcase
-  when 'add'
+  case op
+  when '1'
     "Adding"
-  when 'subtract'
+  when '2'
     "Subtracting"
-  when 'multiply'
+  when '3'
     "Multiplying"
-  when 'divide'
+  when '4'
     "Dividing"
   end
 end
-num1 = 
-num2 = 
 
-prompt("Welcome! I am a calculator. Enter your name:")
-
+num1 = ''
+num2 = ''
+prompt(MESSAGES['welcome'])
+name = ''
 loop do
   name = Kernel.gets().chomp()
-  
-  if name.empty?()
-    prompt("Make sure to use a valid name.")
-  else
-    break
-  end
+  break if name.empty? == false
+  prompt("Make sure to use a valid name.")
 end
 
-loop do #main loop
+# main loop
 loop do
-  prompt("Please input a number.")
-  num1 = gets.chomp.to_i
-  
-  if validate_number?(num1)
-    break
-  else
+  loop do
+    prompt("Please input a number.")
+    num1 = gets.chomp
+
+    break unless validate_number?(num1) == false
     prompt("Hmmm....that doesn't loop like a valid number.")
   end
-end
 
-loop do
-  prompt("Please input another number.")
-  num2 = gets.chomp.to_i
-  
-  if validate_number?(num2)
-    break
-  else
+  loop do
+    prompt("Please input another number.")
+    num2 = gets.chomp
+
+    break if validate_number?(num2)
     prompt("Hmmm....that doesn't loop like a valid number.")
   end
-end
-oper_prompt = <<-MSG
-  What operation would you like me to perform?
-MSG
 
-prompt(oper_prompt)
-oper = ''
+  oper_prompt = <<-MSG
+    What operation would you like me to perform? 
+    1) add
+    2) subtract 
+    3) multiply
+    4) divide
+  MSG
 
-loop do
-  array = ['add', 'subtract', 'multiply', 'divide']
-  oper = Kernel.gets().chomp()
-  if  array.include?(oper)
-    break
-  else
-    prompt("Please enter a valid operation.")
+  prompt(oper_prompt)
+  oper = ''
+
+  loop do
+    oper = Kernel.gets().chomp()
+     break if %w(1 2 3 4).include?(oper)
+     prompt("Please enter a valid operation.")
   end
+
+  prompt("#{oper_to_message(oper)} your two numbers.")
+  sleep 1
+
+  result = case oper
+           when '1'
+             num1.to_i + num2.to_i
+           when '2'
+             num1.to_i - num2.to_i
+           when '3'
+             num1.to_i * num2.to_i
+           when '4'
+             num1.to_f / num2.to_f
+           end
+
+  prompt("Your solution is: #{result}.")
+  prompt("Do you want to perform another calculation?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+
+  prompt("Goodbye!")
 end
-
-prompt("#{oper_to_message(oper)} your two numbers.")
-sleep 1
-
-result = case oper.downcase
-  when 'add'
-    num1 + num2
-  when 'subtract'
-    num1 - num2
-  when 'multiply'
-    num1 * num2
-  when 'divide'
-    num1.to_f / num2.to_f
-end
-
-prompt("Your solution is: #{result}.")
-prompt("Do you want to perform another calculation?")
-answer = Kernel.gets().chomp()
-break unless answer.downcase().start_with?('y')
-
-prompt("Goodbye!")
-end
-  
